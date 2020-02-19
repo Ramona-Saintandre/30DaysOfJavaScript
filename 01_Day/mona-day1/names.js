1caa16e38aa0216117c77a3bcfd7cb30b6a8cccb
Ramona Saintandre <codinggrandma@gmail.com>
2020-02-18 21:07:51 -0500

    completed age.js

---
+++ 01_Day/mona-day1/names.js
@@ -0,0 +1,27 @@
+/*declare variables and store 
+first name
+last name
+marital status
+country
+age on seperate lines*/
+
+
+let firstName = "Ramona";
+let lastName = "SaintAndre";
+let maritalstat = "true";
+let country = "United States ";
+let age = 49;
+
+
+/*declare variables and store 
+first name
+last name
+marital status
+country
+age on single line*/
+
+let firstName1 = "Ramona", lastName1 = "SaintAndre", maritalstat1 = true, country1 = "United States", age1 = 49;
+
+let myAge = `I'm ${age} years old` ; 
+let yourAge = " and, You are 45 years old ";
+console.log (myAge + yourAge);
