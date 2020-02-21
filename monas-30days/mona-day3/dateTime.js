8a78b4e2d5485a879e6541e0db8feafa42252daa
Ramona Saintandre <codinggrandma@gmail.com>
2020-02-20 21:53:06 -0500

    created triangle file

--- monas-30days/mona-day3/dateTime.js
+++ monas-30days/mona-day3/dateTime.js
@@ -46,29 +46,7 @@
 // What is the hours now?
 // What is the minutes now?
 // Find out the numbers of seconds elapsed from January 1, 1970 to now.
-// Exercises: Level 2
-// Write a script that prompt the user to enter base and height of the triangle and calculate an area of a triangle (area = 0.5 x b x h).
 
-// Enter base: 20
-// Enter height: 10
-// The area of the triangle is 50
-// Write a script that prompt the user to enter side a, side b, and side c of the triangle and and calculate the perimeter of triangle (perimeter = a + b + c)
-
-// Enter side a: 5
-// Enter side b: 4
-// Enter side c: 3
-// The perimeter of the triangle is 12
-// Get length and width using prompt and calculate an area of rectangle (area = length x width and the perimeter of rectangle (perimeter = 2 x (length + width))
-
-// Get radius using prompt and calculate the area of a circle (area = pi x r x r) and circumference of a circle(c = 2 x pi x r) where pi = 3.14.
-
-// Calculate the slope, x-intercept and y-intercept of y = 2x -2
-
-// Slope is (m = y2-y1/x2-x1). Find the slope between point (2, 2) and point(6,10)
-
-// Compare the slope of above two questions.
-
-// Calculate the value of y (y = x^2 + 6x + 9). Try to use different x values and figure out at what x value y is 0.
 
 // Writ a script that prompt a user to enters hours and rate per hour. Calculate pay of the person?
 
