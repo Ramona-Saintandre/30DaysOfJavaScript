343cf41e23a42dd2b58e7df77514a07e64b9dcfb
Ramona Saintandre <codinggrandma@gmail.com>
2020-02-24 07:45:18 -0500

    need to finish this challenge

--- monas-30days/mona-day2/datatypes.js
+++ monas-30days/mona-day2/datatypes.js
@@ -2,35 +2,85 @@
 
 
 // String Part
-// Declare a variable name challenge and assign it to an initial value '30 Days Of JavaScript'.
-// Print the string on the browser console using console.log()
-// Print the length of the string on the browser console using console.log()
-// Change all the string to capital letters using toUpperCase() method
-// Change all the string to lowercase letters using toLowerCase() method
-// // Cut(slice) out the first word of the string using substr() or substring() method
+// 1.Declare a variable name challenge and assign it to an initial value '30 Days Of JavaScript'.
+let challenge = '30 Days of JavaScript'
+// 2.Print the string on the browser console using console.log()
+console.log(challenge);
+// 3.Print the length of the string on the browser console using console.log()
+console.log(challenge.length)
+// 4.Change all the string to capital letters using toUpperCase() method
+console.log(challenge.toUpperCase());
+// 5.Change all the string to lowercase letters using toLowerCase() method
+console.log(challenge.toLowerCase());
+// 6.Cut(slice) out the first word of the string using substr() or substring() method
+console.log(challenge.substr(0,2));
+// 7. Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
+console.log(challenge.substring(3));
+// 7. Use substr to slice out the phrase because because because in the following sentence:
+//'You cannot end a sentence with because because because is a conjunction'
+console.log
+// 8.Check if the string contains a word Script using includes() method
+console.log(challenge.includes);
+// 9.Split the string into array using split() method
+console.log(challenge.split());
+// 10.Split the string 30 Days Of JavaScript at the space using split() method
+console.log(challenge.split(''));
+// 11.'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+let company = ('google','microsoft','apple','IBM','Oracle','Amazon');
+console.log(company.brandsArray.split(','));
+// 12. Change 30 Days Of JavaScript to Microsoft Academy using replace() method.
+console.log(challenge.replace('JavaScript','Python'))
+// 13.What is character at index 15 in '30 Days Of JavaScript' string use charAt() method.
+console.log(challenge.charAt(15));
+// 14.What is the character code of J in '30 Days Of JavaScript' string using charCodeAt()
+console.log(challenge.charAt(11));
+// 15.Use indexOf to determine the position of the first occurrence of a in 30 Days Of JavaScript
+console.log
+// 16.Use lastIndexOf to determine the position of the last occurrence of a in 30 Days Of JavaScript.
+console.log
+// 17.Use indexOf to find the position of the first occurrence of the word because in the following sentence:
+//'You cannot end a sentence with because because because is a conjunction'
 
-// // Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
 
-// // Use substr to slice out the phase because because because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
-// //Check if the string contains a word Script using includes() method
-// Split the string into array using split() method
-// Split the string 30 Days Of JavaScript at the space using split() method
-// 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
-// Change 30 Days Of JavaScript to Microsoft Academy using replace() method.
-// What is character at index 15 in '30 Days Of JavaScript' string use charAt() method.
-// What is the character code of J in '30 Days Of JavaScript' string using charCodeAt()
-// Use indexOf to determine the position of the first occurrence of a in 30 Days Of JavaScript
-// Use lastIndexOf to determine the position of the last occurrence of a in 30 Days Of JavaScript.
-// Use indexOf to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
-// Use lastIndexOf to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
-// Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
-// Use trim() to remove if there is trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
-// Use startsWith() method with the string 30 Days Of JavaScript make the result true
-// Use endsWith() method with the string 30 Days Of JavaScript make the result true
-// Use match() method to find all the a’s in 30 Days Of JavaScript
-// Use match() to count the number all because's in the following sentence:'You cannot end a sentence with because because because is a conjunction'
-// Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
-// Use repeat() method to print 30 Days Of JavaScript 2 times
-// Love is the best thing in this world. Some found their love and some are still looking for their love. Count the number of word love in this sentence.
-// Calculate the total annual income of the person by extract the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+console.log
+// 18.Use lastIndexOf to find the position of the first occurrence of the word because in the following sentence:
+//'You cannot end a sentence with because because because is a conjunction'
+
+
+console.log
+// 19Use search to find the position of the first occurrence of the word because in the following sentence:
+//'You cannot end a sentence with because because because is a conjunction'
+
+
+console.log
+// 20.Use trim() to remove if there is trailing whitespace at the beginning and the end of a string.E.g 
+//' 30 Days Of JavaScript '.
+
+console.log
+// 21.Use startsWith() method with the string 30 Days Of JavaScript make the result true
+console.log
+// 22.Use endsWith() method with the string 30 Days Of JavaScript make the result true
+console.log
+// 23.Use match() method to find all the a’s in 30 Days Of JavaScript
+console.log
+// 24.Use match() to count the number all because's in the following sentence:
+//'You cannot end a sentence with because because because is a conjunction'
+
+
+console.log
+// 25.Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
+
+
+console.log
+// 26.Use repeat() method to print 30 Days Of JavaScript 2 times
+console.log(challenge.repeat(2));
+
+// Love is the best thing in this world. 
+//Some found their love and some are still looking for their love. Count the number of word love in this sentence.
+
+// 27.Calculate the total annual income of the person by extract the numbers from the following text.
+//'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+
+console.log
+
 // Clean the following text(hint, use replace and regular express)
