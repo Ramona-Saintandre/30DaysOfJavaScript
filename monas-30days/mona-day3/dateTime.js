f76d750146c8dd0cc2fa3ae8919874429e6b1cf3
Ramona Saintandre <codinggrandma@gmail.com>
2020-02-24 07:55:50 -0500

    set up challenge 3

--- monas-30days/mona-day3/dateTime.js
+++ monas-30days/mona-day3/dateTime.js
@@ -1,49 +1,79 @@
 // Day 3: Exercises
 
-// Use the Date object to do the following activities
+// 1.Use the Date object to do the following activities
 
-// What is the year today?
-// What is the month today as a number?
-// What is the date today?
-// What is the day today as a number?
-// What is the hours now?
-// What is the minutes now?
-// Find out the numbers of seconds elapsed from January 1, 1970 to now.
+// 2.What is the year today?
+
+// 3.What is the month today as a number?
+
+// 4.What is the date today?
+
+// 5.What is the day today as a number?
+
+// 6.What is the hours now?
+
+// 7.What is the minutes now?
+
+// 8.Find out the numbers of seconds elapsed from January 1, 1970 to now.
 
 
-// Writ a script that prompt a user to enters hours and rate per hour. Calculate pay of the person?
+
+// 9.Write a script that prompt a user to enters hours and rate per hour. 
+//Calculate pay of the person?
 
 // Enter hours: 40
 // Enter rate per hour: 28
 // Your weekly earning is 1120
-// If the length of your name is greater than 7 say, your name is long else say your name is short.
 
-// Compare your first name length and your family name length and you should get this output.
 
-// let firstName = 'Asabeneh'
-// let lastName = 'Yetayeh'
-// Your first name, Asabeneh is longer than your family name, Yetayeh
-// Declare two variables myAge and yourAge and assign them initial values and myAge and yourAge.
 
+// 10.If the length of your name is greater than 7 say,
+// your name is long else say your name is short.
+
+
+
+// 11.Compare your first name length and your family name length 
+// let firstName = 'Ramona'
+// let lastName = 'Saintandre'
+//and you should get this output.
+// Your first name, Ramona is longer than your family name, Saintandre
+
+
+
+
+
+// 12.Declare two variables myAge and yourAge and assign them initial values and myAge and yourAge.
 // let myAge = 250
 // let yourAge = 25
 // I am 225 years older than you.
-// Using prompt get the year the user was born and if the user is 18 or above allow the user to drive if not tell the user to wait a certain amount of years.
+
+
+// 13.Using prompt get the year the user was born and if the user is 18 or above allow the user to drive if not tell the user to wait a certain amount of years.
 
 // Enter birth year: 1995
 // You are 25. You are old enough to drive
 
 // Enter birth year: 2005
 // You are 15. You will be allowed to drive after 3 years.
-// Write a script that prompt the user to enter number of years. Calculate the number of seconds a person can live. Assume some one lives just hundred years
 
-// Enter number of yours you live: 100
+
+
+
+// 14.Write a script that prompt the user to enter number of years. Calculate the number of seconds a person can live. Assume some one lives just hundred years
+
+
+
+// 15.Enter number of yours you live: 100
 // You lived 3153600000 seconds.
 // Create a human readable time format using the Date time object
+
+
 
 // YYY-MM-DD HH:mm
 // DD-MM-YYYY HH:mm
 // DD/MM/YYY HH:mm
+
+
 
 
 // Exercises: Level 3
